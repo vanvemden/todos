375c3167f1938570e6685fe25195d6c39615edb3
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 15:47:23 -0700

    add prop and style to component

--- app/components/Todo/index.js
+++ app/components/Todo/index.js
@@ -5,13 +5,23 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import Wrapper from './Wrapper';
+import TodoText from './TodoText';
+import TodoStatus from './TodoStatus';
 // import styled from 'styled-components';
 
-function Todo() {
-  return <div />;
+function Todo({ todo }) {
+  return (
+    <Wrapper>
+      <TodoText>{todo.text}</TodoText>
+      <TodoStatus>{todo.done}</TodoStatus>
+    </Wrapper>
+  );
 }
 
-Todo.propTypes = {};
+Todo.propTypes = {
+  todo: PropTypes.object.isRequired,
+};
 
 export default Todo;
