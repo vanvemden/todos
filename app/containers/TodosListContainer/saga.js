be236c6fc181e0a298e0820900afe532b6808946
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 13:06:05 -0700

    add request todos saga

--- app/containers/TodosListContainer/saga.js
+++ app/containers/TodosListContainer/saga.js
@@ -1,6 +1,12 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
+import { REQUEST_TODOS } from './constants';
+
+function* requestTodos() {
+  console.log('requesting todos from api');
+}
 
 // Individual exports for testing
-export default function* todosListContainerSaga() {
-  // See example in containers/HomePage/saga.js
+export default function* requestTodosSaga() {
+  yield takeLatest(REQUEST_TODOS, requestTodos);
 }
