570947dde2c9c48d6d1aaa08cf94c709d81f378e
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 11:38:26 -0700

    add request actions

--- app/containers/TodosListContainer/actions.js
+++ app/containers/TodosListContainer/actions.js
@@ -4,10 +4,28 @@
  *
  */
 
-import { DEFAULT_ACTION } from './constants';
+import {
+  REQUEST_TODOS,
+  REQUEST_TODOS_SUCCEEDED,
+  REQUEST_TODOS_FAILED,
+} from './constants';
 
-export function defaultAction() {
+export function requestTodos() {
   return {
-    type: DEFAULT_ACTION,
+    type: REQUEST_TODOS,
+  };
+}
+
+export function requestTodosSucceeded(todos) {
+  return {
+    type: REQUEST_TODOS_SUCCEEDED,
+    todos,
+  };
+}
+
+export function requestTodosFailed(message) {
+  return {
+    type: REQUEST_TODOS_FAILED,
+    message,
   };
 }
