7119a3cb893a2c6e21677b5ab6c1014c2de7dde2
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 11:33:22 -0700

    add request action types

--- app/containers/TodosListContainer/constants.js
+++ app/containers/TodosListContainer/constants.js
@@ -4,4 +4,8 @@
  *
  */
 
-export const DEFAULT_ACTION = 'app/TodosListContainer/DEFAULT_ACTION';
+export const REQUEST_TODOS = 'app/TodosListContainer/REQUEST_TODOS';
+export const REQUEST_TODOS_SUCCEEDED =
+  'app/TodosListContainer/REQUEST_TODOS_SUCCEEDED';
+export const REQUEST_TODOS_FAILED =
+  'app/TodosListContainer/REQUEST_TODOS_FAILED';
