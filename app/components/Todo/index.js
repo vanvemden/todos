0d2bd6127b404feb88423bc04e748884b416b63e
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 14:16:11 -0700

    generate boilerplate for Todo component

---
+++ app/components/Todo/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Todo
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Todo() {
+  return <div />;
+}
+
+Todo.propTypes = {};
+
+export default Todo;
