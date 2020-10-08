a5bcf240a8b273274ee3b847e20ded9bc88eb2f7
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 14:07:31 -0700

    generate boilerplate for TodoList component

---
+++ app/components/TodoList/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * TodoList
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function TodoList() {
+  return <div />;
+}
+
+TodoList.propTypes = {};
+
+export default TodoList;
