9682a1820cd0fd49715ceabcaeb919f732defb6e
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 13:07:51 -0700

    render TodoListContainer on root route

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -10,7 +10,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
+import TodosListContainer from 'containers/TodosListContainer/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 import GlobalStyle from '../../global-styles';
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={TodosListContainer} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
