0d37a72c8edc2d8ea6a542c9e3634d3c08c5fad0
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 22:22:52 -0700

    make linter happy

--- app/containers/TodoFormContainer/constants.js
+++ app/containers/TodoFormContainer/constants.js
@@ -5,7 +5,7 @@
  */
 
 export const SUBMIT_TODO = 'app/TodoFormContainer/SUBMIT_TODO';
-export const SUBMIT_TODO_FAILED ='app/TodoFormContainer/SUBMIT_TODO_FAILED';
+export const SUBMIT_TODO_FAILED = 'app/TodoFormContainer/SUBMIT_TODO_FAILED';
 export const SUBMIT_TODO_SUCCEEDED =
   'app/TodoFormContainer/SUBMIT_TODO_SUCCEEDED';
 export const SUBMIT_TODO_CANCELLED =
