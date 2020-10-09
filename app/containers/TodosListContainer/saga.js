d440a1d586c829c56e8e39c6ad83235a96b3e470
Marco van Vemden <vanvemden@gmail.com>
2020-10-08 18:35:29 -0700

    add generator and static data for testing

--- app/containers/TodosListContainer/saga.js
+++ app/containers/TodosListContainer/saga.js
@@ -1,12 +1,22 @@
-// import { take, call, put, select } from 'redux-saga/effects';
-import { takeLatest } from 'redux-saga/effects';
+import { put, takeLatest } from 'redux-saga/effects';
 import { REQUEST_TODOS } from './constants';
+import { requestTodosSucceeded, requestTodosFailed } from './actions';
+
+const todos = [
+  { id: '1', text: 'Hello World!', checked: false },
+  { id: '2', text: 'Hello World 1!', checked: true },
+  { id: '3', text: 'Hello World 2!', checked: false },
+  { id: '4', text: 'Hello World 3!', checked: true },
+];
 
 function* requestTodos() {
-  console.log('requesting todos from api');
+  try {
+    yield put(requestTodosSucceeded(todos));
+  } catch (err) {
+    yield put(requestTodosFailed(err.message));
+  }
 }
 
-// Individual exports for testing
 export default function* requestTodosSaga() {
   yield takeLatest(REQUEST_TODOS, requestTodos);
 }
