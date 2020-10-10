7fe4dcd20c21b39a35af58f5e5dcc91ab9b8ba1b
Marco van Vemden <vanvemden@gmail.com>
2020-10-10 15:08:54 -0700

    flesh out todo form component

--- app/components/TodoForm/index.js
+++ app/components/TodoForm/index.js
@@ -6,22 +6,49 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
-
 import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
-function TodoForm({ text }) {
+import messages from './messages';
+import TextInput from '../TextInput';
+import Button from '../Button';
+import Form from '../Form';
+
+function TodoForm({
+  todoText,
+  onTodoTextChange,
+  onTodoFormSubmit,
+  onTodoFormCancel,
+}) {
+  const handleFormSubmit = evt => {
+    evt.preventDefault();
+    onTodoFormSubmit({
+      text: todoText,
+    });
+  };
+
   return (
     <div>
       <FormattedMessage {...messages.header} />
-      <input placeholder="Description" value={text} type="text" />
+      <Form onSubmit={handleFormSubmit}>
+        <TextInput
+          placeholder="What needs to be done..."
+          onChange={onTodoTextChange}
+          value={todoText}
+          type="text"
+          id="text"
+        />
+        <Button bgColor="red" label="Add" onClick={handleFormSubmit} />
+        <Button bgColor="green" label="Cancel" onClick={onTodoFormCancel} />
+      </Form>
     </div>
   );
 }
 
 TodoForm.propTypes = {
-  text: PropTypes.string,
+  todoText: PropTypes.string.isRequired,
+  onTodoTextChange: PropTypes.func.isRequired,
+  onTodoFormSubmit: PropTypes.func.isRequired,
+  onTodoFormCancel: PropTypes.func.isRequired,
 };
 
 export default TodoForm;
