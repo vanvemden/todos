1592653232341e9eb7cf6154e5641f4477cf06d9
Marco van Vemden <vanvemden@gmail.com>
2020-10-11 10:29:28 -0700

    load api routes in middleware

--- server/index.js
+++ server/index.js
@@ -14,8 +14,15 @@ const ngrok =
 const { resolve } = require('path');
 const app = express();
 
+const apiRoutes = require('./routes/api');
+
+// For processing JSON:
+app.use(express.json());
+
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
+
+app.use('/api', apiRoutes);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
