0e080ac8617d763196c082b8cb422e84ba0d3b71
Marco van Vemden <vanvemden@gmail.com>
2020-10-10 19:22:25 -0700

    ignore api errors and continue till api works

--- app/containers/TodoFormContainer/saga.js
+++ app/containers/TodoFormContainer/saga.js
@@ -1,21 +1,25 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { put, call, takeLatest } from 'redux-saga/effects';
 import uuid from 'react-uuid';
-import { submitTodoFailed, submitTodoSucceeded } from './actions';
+import { submitTodoSucceeded } from './actions';
 import { SUBMIT_TODO } from './constants';
 import { postTodoApi } from '../../api';
 
 function* submitTodo(action) {
-  const todo = action.todo;
-  todo.id = uuid();
-
   try {
-    yield call(postTodoApi, todo);
+    const todo = yield call(postTodoApi, action.todo);
     yield put(submitTodoSucceeded(todo));
   } catch (error) {
-    console.log('Error but continuing', error);
-    yield put(submitTodoSucceeded(todo));
+    // TODO: Uncomment below & load 'submitTodoFailed' when api works
     // yield put(submitTodoFailed(error.message));
+
+    // TODO: Remove code below when api works
+    const tempTodo = {
+      id: uuid(),
+      text: action.todo.text,
+      checked: false,
+    };
+    yield put(submitTodoSucceeded(tempTodo));
   }
 }
 // Individual exports for testing
