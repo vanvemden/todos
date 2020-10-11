f753a0474b9f1e252d41541f16c961ab41a9f754
Marco van Vemden <vanvemden@gmail.com>
2020-10-11 14:28:22 -0700

    add api fn to toggle the checked boolean by todo id

--- app/api/index.js
+++ app/api/index.js
@@ -1,10 +1,18 @@
-export function getTodosApi() {
+/**
+ *
+ * Api
+ *
+ */
+
+/* Api call to fetch all todos */
+export function apiFetchTodos() {
   return fetch(`http://localhost:3000/api/todos/`).then(response =>
     response.json(),
   );
 }
 
-export function postTodoApi({ todo }) {
+/* Api call to create todo */
+export function apiCreateTodo(todo) {
   return fetch(`http://localhost:3000/api/todos/`, {
     method: 'POST',
     headers: {
@@ -14,5 +22,12 @@ export function postTodoApi({ todo }) {
     body: JSON.stringify({
       todo,
     }),
+  }).then(response => response.json());
+}
+
+/* Api call to toggle checked boolean */
+export function apiToggleChecked(id) {
+  return fetch(`http://localhost:3000/api/todos/${id}/checked`, {
+    method: 'PATCH',
   }).then(response => response.json());
 }
