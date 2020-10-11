8611c7c0157225bc0dd651931bec29cf45882816
Marco van Vemden <vanvemden@gmail.com>
2020-10-10 19:45:33 -0700

    import Roboto font and set some styling

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+
+  @import url(https://fonts.googleapis.com/css?family=Roboto:400,300,500,700);
+
   html,
   body {
     height: 100%;
@@ -8,24 +11,19 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-family: Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;
   }
 
   body.fontLoaded {
-    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-family: Roboto, 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
   }
 
   #app {
-    background-color: #fafafa;
+    background-color: papayawhip;
     min-height: 100%;
     min-width: 100%;
   }
 
-  p,
-  label {
-    font-family: Georgia, Times, 'Times New Roman', serif;
-    line-height: 1.5em;
-  }
 `;
 
 export default GlobalStyle;
