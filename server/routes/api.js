03079e5d4981b7df0af872a278c98de7dfb12cbc
Marco van Vemden <vanvemden@gmail.com>
2020-10-11 14:23:56 -0700

    fix api route for todo checked toggle

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,7 +9,7 @@ const Todo = require('../models/Todo');
 
 const router = new express.Router();
 
-/* GET api/todos: Fetch all todos in db */
+/* GET api/todos: fetch all todos in db */
 router.get('/todos', async function geTodos(req, res, next) {
   try {
     const todos = await Todo.getAll();
@@ -19,7 +19,7 @@ router.get('/todos', async function geTodos(req, res, next) {
   }
 });
 
-/* POST api/todos: Save a new todo to db */
+/* POST api/todos: save a new todo to db */
 router.post('/todos', async function postTodo(req, res, next) {
   try {
     const todo = await Todo.create(req.body.todo);
@@ -29,11 +29,14 @@ router.post('/todos', async function postTodo(req, res, next) {
   }
 });
 
-/* POST api/todos: Save a new todo to db */
-router.patch('/todos/:id/checked', async function patchTodoChecked(req, res, next) {
+/* PATCH api/todos/:id/checked: toggle checked boolean in db */
+router.patch('/todos/:todoId/checked', async function patchTodoChecked(
+  req,
+  res,
+  next,
+) {
   try {
-    const todo = await Todo.toggleChecked(req.params.id);
-    console.log('toggleChecked', todo);
+    const todo = await Todo.toggleChecked(req.params.todoId);
     return res.status(201).json({ todo });
   } catch (err) {
     return next(err);
