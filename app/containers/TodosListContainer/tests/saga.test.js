4d728e88e0047196dddc0a42c8af9015c826ce43
Marco van Vemden <vanvemden@gmail.com>
2020-10-12 18:28:05 -0700

    update TodosListContainer saga tests

--- app/containers/TodosListContainer/tests/saga.test.js
+++ app/containers/TodosListContainer/tests/saga.test.js
@@ -1,15 +1,57 @@
 /**
- * Test sagas
+ * Tests for TodosListContainer sagas
  */
 
+import { put, takeLatest } from 'redux-saga/effects';
+import { REQUEST_TODOS, TOGGLE_TODO } from '../constants';
+import { requestTodosSucceeded, requestTodosFailed } from '../actions';
+import todosListContainerSaga, {
+  requestTodos,
+  toggleTodoChecked,
+} from '../saga';
+
 /* eslint-disable redux-saga/yield-effects */
-// import { take, call, put, select } from 'redux-saga/effects';
-// import todosListContainerSaga from '../saga';
+describe('requestTodos Saga', () => {
+  let requestTodosGenerator;
 
-// const generator = todosListContainerSaga();
+  beforeEach(() => {
+    /* Handle call to the api to fetch todos */
+    requestTodosGenerator = requestTodos();
+    const callDescriptor = requestTodosGenerator.next().value;
+    expect(callDescriptor).toMatchSnapshot();
+  });
 
-describe('todosListContainerSaga Saga', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('should dispatch requestTodosSucceeded on api success', () => {
+    /* Mock response object from api call */
+    const response = {
+      todos: [{ id: '12345', text: 'Testing', checked: false }],
+    };
+    const putDescriptor = requestTodosGenerator.next(response).value;
+    expect(putDescriptor).toEqual(put(requestTodosSucceeded(response.todos)));
+  });
+
+  it('should dispatch requestTodosFailed on api error', () => {
+    /* Mock response from catching api error */
+    const response = new Error('Error message');
+    const putDescriptor = requestTodosGenerator.throw(response).value;
+    expect(putDescriptor).toEqual(put(requestTodosFailed(response.message)));
+  });
+});
+
+describe('todosListContainerSaga', () => {
+  const todosListContainerGenerator = todosListContainerSaga();
+
+  it('should start task to watch for REQUEST_TODOS action', () => {
+    const takeLatestDescriptor = todosListContainerGenerator.next().value;
+    expect(takeLatestDescriptor).toEqual(
+      takeLatest(REQUEST_TODOS, requestTodos),
+    );
+  });
+
+  it('should start task to watch for TOGGLE_TODO action', () => {
+    const takeLatestDescriptor = todosListContainerGenerator.next().value;
+    expect(takeLatestDescriptor).toEqual(
+      takeLatest(TOGGLE_TODO, toggleTodoChecked),
+    );
   });
 });
