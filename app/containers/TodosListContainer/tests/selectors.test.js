01517439251c48ddddf8c3099382478e986db6f6
Marco van Vemden <vanvemden@gmail.com>
2020-10-12 21:16:38 -0700

    add TodosListContainer selectors tests

--- app/containers/TodosListContainer/tests/selectors.test.js
+++ app/containers/TodosListContainer/tests/selectors.test.js
@@ -1,7 +1,60 @@
-// import { selectTodosListContainerDomain } from '../selectors';
+/**
+ * Tests for TodosListContainer selectors
+ */
+
+import makeSelectTodosListContainer, {
+  selectTodosListContainerDomain,
+  makeSelectTodos,
+  makeSelectTodosCount,
+} from '../selectors';
+
+describe('makeSelectTodosListContainer', () => {
+  const todoListContainerSelector = makeSelectTodosListContainer();
+  it('should return the TodosListContainer state', () => {
+    const domainState = {
+      todos: [],
+    };
+    const mockedState = {
+      todosListContainer: domainState,
+    };
+    expect(todoListContainerSelector(mockedState)).toEqual(domainState);
+  });
+});
 
 describe('selectTodosListContainerDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('should select the TodosListContainerDomain state', () => {
+    const domainState = {
+      todos: [],
+    };
+    const mockedState = {
+      todosListContainer: domainState,
+    };
+    expect(selectTodosListContainerDomain(mockedState)).toEqual(domainState);
+  });
+});
+
+describe('makeSelectTodos', () => {
+  const todosSelector = makeSelectTodos();
+  it('should select the todos', () => {
+    const todos = [];
+    const mockedState = {
+      todosListContainer: {
+        todos,
+      },
+    };
+    expect(todosSelector(mockedState)).toEqual(todos);
+  });
+});
+
+describe('makeSelectTodosCount', () => {
+  const todosCountSelector = makeSelectTodosCount();
+  it('should return the count of unchecked todos', () => {
+    const todos = [{ id: 1, text: 'Testing', checked: false }];
+    const mockedState = {
+      todosListContainer: {
+        todos,
+      },
+    };
+    expect(todosCountSelector(mockedState)).toEqual(todos.length);
   });
 });
