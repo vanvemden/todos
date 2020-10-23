633304f25d393dacc99f5b74a2ef0db52fb16d98
Marco van Vemden <vanvemden@gmail.com>
2020-10-22 21:15:02 -0700

    move Button component styling into index.js

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -8,10 +8,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
-import buttonStyles from './styles';
 
 const StyledButton = styled.button`
-  ${buttonStyles};
+  color: #424147;
+  display: inline-flex;
+  line-height: 1em;
+  font-size: 1.25em;
+  margin: 0.75em 0.75em 0 0;
+  padding: 0.25em 1em;
+  border-radius: 0.25rem;
+  border-width: 1px;
+  border-style: dashed;
+  border-color: ${props => props.bgColor};
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ffffff;
+  }
 `;
 
 function Button({ message, bgColor, onClick }) {
