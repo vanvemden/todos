5c1e364c28d561768d7b32fed1692bb4fcd8de2a
Marco van Vemden <vanvemden@gmail.com>
2020-10-22 21:20:07 -0700

    move TextInput component styling into index.js

--- app/components/TextInput/index.js
+++ app/components/TextInput/index.js
@@ -7,14 +7,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { inputStyles, errorStyles } from './styles';
 
 const StyledInput = styled.input`
-  ${inputStyles};
+  display: block;
+  width: 100%;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px dashed #b4b4b2;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 `;
 
 const StyledError = styled.div`
-  ${errorStyles};
+  display: block;
+  padding: 0.375rem 0.75rem 0;
+  margin-top: 4px;
+  font-size: 0.9rem;
+  line-height: 1;
+  color: red;
 `;
 
 function TextInput({ placeholder, onChange, value, type, id, error }) {
